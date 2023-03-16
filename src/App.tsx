fe4b783ca8e25e4e5ba04509fe5f9589c4e3a645
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-03-16 06:24:39 +0200

    git init

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+let a : number | null = 10;
+let name: string = "it-proger";
+name = "it-proger";
+
+let sex: "male" | "female";
+//sex = "man"
+
+let isSamurai: boolean | null = true;
+isSamurai = null;
+
+let names: Array<string> = ["Kolya", "Dimych", "Valera"];
+// alert( names[0].toUpperCase());
+names.forEach(n =>{
+  // alert(n)
+});
+
+type AddressType = {
+  city?: string
+  country: string
+}
+
+type UserType = {
+  sayHello: (message: string) => void
+  name: string
+  age: number
+  isSamurai: boolean
+  address: AddressType | null
+}
+
+let user: UserType ={ 
+  sayHello(message: string) {alert('yo')},
+  name: 'Kolya',
+  age: 2,
+  isSamurai:true,
+  address: {
+    country: "Ukraine"
+  }
+}
+
+console.log(user.sayHello("yo yo"));
+
+const summ = (a:any , b:any ) => {
+  return a + b;
+}
+
+summ(10, "20")
+
+console.log(summ(10, "20"));
+
+
+
 function App() {
   return (
     <div className="App">
