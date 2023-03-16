39ac07b1a622ce946bcdce944e4bca070b082faf
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-03-16 18:18:24 +0200

    add typeof

--- src/App.tsx
+++ src/App.tsx
@@ -20,29 +20,43 @@ names.forEach(n =>{
 });
 
 type AddressType = {
-  city?: string
-  country: string
+  city?: string | null
+  country: string | null
 }
 
-type UserType = {
-  sayHello: (message: string) => void
-  name: string
-  age: number
-  isSamurai: boolean
-  address: AddressType | null
+
+
+let initialState = { 
+  name: null as string | null,
+  age: null as number | null, 
+  isSamurai: null as boolean | null,
+  addresses: [] as Array<AddressType>, 
+  counter: 0
 }
 
-let user: UserType ={ 
-  sayHello(message: string) {alert('yo')},
-  name: 'Kolya',
-  age: 2,
-  isSamurai:true,
-  address: {
-    country: "Ukraine"
-  }
+export type InitialStateType = typeof initialState;
+let state: InitialStateType = {
+    addresses: [{city: "rrcrc", country: "crcr"}],
+    age: null,
+    counter: 10,
+    isSamurai: true,
+    name: "Kolya"
 }
 
-console.log(user.sayHello("yo yo"));
+
+let GET_TASKS = "APP/GetTASKS";
+type GetTasksActionType = {
+  id: number,
+  type: typeof GET_TASKS
+}
+
+let action: GetTasksActionType = {
+  type: GET_TASKS,
+  id: 12
+}
+
+
+// console.log(user.sayHello("yo yo"));
 
 const summ = (a:any , b:any ) => {
   return a + b;
